e0b091fab18d5b19121ff2fa16dce0407ac0649f
Rongjian Zhang <pd4d10@gmail.com>
2024-02-17 01:01:34 +0800

    fix: add blocklist to make build work

--- scripts/gen-res.js
+++ scripts/gen-res.js
@@ -8,10 +8,21 @@ fg.sync("src/*.res", { absolute: true }).forEach((file) => {
   fs.unlinkSync(file);
 });
 
+const blockedModule = ["JSON", "Type"];
+
+/** @type {Record<string, (string | RegExp)[]>} */
+const blockedFn = {
+  Array: ["swapUnsafe"],
+  List: [/Aux/],
+};
+
 fg.sync("./node_modules/@rescript/core/src/*.mjs", {
   absolute: true,
 }).forEach((file) => {
   if (!file.includes("__")) return;
+
+  const moduleName = file.split("__")[1].split(".")[0];
+  if (blockedModule.includes(moduleName)) return;
 
   const content = fs.readFileSync(file, { encoding: "utf-8" });
   const fnNames = js
@@ -22,7 +33,12 @@ fg.sync("./node_modules/@rescript/core/src/*.mjs", {
       const name = v.getMatch("NAME")?.text();
       return name &&
         // invalid name
-        !name.startsWith("$$")
+        !name.startsWith("$$") &&
+        !blockedFn[moduleName]?.some((regexOrStr) =>
+          typeof regexOrStr === "string"
+            ? name === regexOrStr
+            : regexOrStr.test(name),
+        )
         ? [name]
         : [];
     });
@@ -30,7 +46,6 @@ fg.sync("./node_modules/@rescript/core/src/*.mjs", {
   // ignore empty files
   if (fnNames.length === 0) return;
 
-  const moduleName = file.split("__")[1].split(".")[0];
   const code = fnNames
     .map((name) => `@genType let ${name} = RescriptCore.${moduleName}.${name}`)
     .join("\n");
